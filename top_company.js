7cf672335c5036b4df3f9d49784cec84f1e6192a
WC-DEVSERVER5\IkemuraK <IkemuraK@kogokojupiterjcomco.onmicrosoft.com>
2017-03-13 14:23:03 +0900

    DB送信を実装

--- top_company.js
+++ top_company.js
@@ -1,6 +1,7 @@
 const firebase = require("firebase");
 const consts = require('./consts.js');
 
+const TABLE_NAME = 'top_company/anei';
 /*
 firebaseから一覧を読み込む
 読み込み完了後、1社ずつステータスを判定して結果を変数に格納
@@ -8,33 +9,78 @@ firebaseから一覧を読み込む
 変数をDBに登録
 */
 
-firebase.database()
-  .ref('/anei_status/')
-  .once('value')
-  .then(function(snapshot) {
-    const statuses = [];
-    snapshot.forEach(function(element) {
-      const portData = element.val();
-      if (portData.code == undefined) return false; //全体コメントはスキップ
-      statuses.push(portData.status.code); // 一旦配列に結果を保存
+/**
+ * DBからanei_statusを取得してstatus.codeのみを取得
+ */
+function readFirebase() {
+  return firebase.database().ref('/anei_status/').once('value')
+    .then(function(snapshot) {
+      const statuses = [];
+      snapshot.forEach(function(element) {
+        const portData = element.val();
+        if (portData.code == undefined) return false; //全体コメントはスキップ
+        statuses.push(portData.status.code); // 一旦配列に結果を保存
+      });
+      return new Promise(function(resolve) {
+        resolve(statuses);
+      })
     });
+}
 
-    console.log(statuses);
-    let comment = '';
-    if (statuses.indexOf(consts.CANCEL) > 0) {
-      //欠航あり
-      comment += '欠航あり';
-    }
-    if (statuses.indexOf(consts.CATION) > 0) {
-      if (comment.length) comment += ' ';
-      //注意あり
-      comment += '注意あり';
-    }
+/**
+ * 配列の運行結果をみて運行コメントを作成する
+ * @param {aneiの運行結果配列} statuses 
+ */
+function createAneiTopStatus(statuses) {
+  // console.log(statuses);
+  let comment = '';
+  if (statuses.indexOf(consts.CANCEL) > 0) {
+    //欠航あり
+    comment += '欠航あり';
+  }
+  if (statuses.indexOf(consts.CATION) > 0) {
+    if (comment.length) comment += ' ';
+    //注意あり
+    comment += '注意あり';
+  }
 
-    if (comment.length == '') {
-      // 欠航も注意も未定もなし＝すべて運行
-      comment = '全便運行';
+  if (comment.length == '') {
+    // 欠航も注意も未定もなし＝すべて運行
+    comment = '全便運行';
+  }
+  const sendData = {
+      comment: comment
     }
-    console.log(comment);
-    firebase.database().goOffline()
-  });
+    // console.log(comment);
+  return Promise.resolve(sendData);
+}
+
+/**
+ * firebaseへ送信
+ * @param {送信する値} sendData 
+ */
+function sendFirebase(sendData) {
+  console.log('DB登録開始');
+  return firebase.database().ref(TABLE_NAME).update(sendData, function() {
+    console.log('DB登録完了');
+    firebase.database().goOffline(); //プロセスが終わらない対策
+  })
+}
+
+/**
+ * 外部からの呼び出し用メソッド
+ */
+function run() {
+  // console.log('top_company 開始');
+  Promise.resolve()
+    .then(() => console.log('top_company 開始'))
+    .then(() => readFirebase())
+    .then(data => createAneiTopStatus(data))
+    .then(sendData => sendFirebase(sendData))
+    .then(() => console.log('top_company 完了'))
+    .catch(function(e) {
+      console.log(e);
+    })
+}
+run()
+module.exports = run;
