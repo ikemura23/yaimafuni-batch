db24cc44135219c84c3b19a18b72007329dbb8b9
ikemura <8417910+banbara23@users.noreply.github.com>
2017-09-26 15:42:40 +0900

    slack通知のコメントアウトを戻し忘れた

--- slack.js
+++ slack.js
@@ -7,18 +7,18 @@ const config = require("./config.json");
 module.exports = function post(msg) {
   console.log('Post to Slack: ' + msg);
 
-  // const webhookUri = config.slack.webHookUrl;
-  // slack = new Slack();
-  // slack.setWebhook(webhookUri);
-  // slack.webhook({
-  //   channel: "#notification",
-  //   username: "yaimafuni-backend",
-  //   icon_emoji: ":ghost:",
-  //   text: getNowFormatDateTime() + '\n' + msg,
-  // }, function(err, response) {
-  //   if (err) console.log(err);
-  //   // console.log(response);
-  // });
+  const webhookUri = config.slack.webHookUrl;
+  slack = new Slack();
+  slack.setWebhook(webhookUri);
+  slack.webhook({
+    channel: "#notification",
+    username: "yaimafuni-backend",
+    icon_emoji: ":ghost:",
+    text: getNowFormatDateTime() + '\n' + msg,
+  }, function(err, response) {
+    if (err) console.log(err);
+    // console.log(response);
+  });
 }
 
 /**
