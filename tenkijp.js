1b798ffb6e696d56b3cd448a1c776a70544616d2
ikemura <8417910+banbara23@users.noreply.github.com>
2018-07-25 00:24:22 +0900

    tenkijpのバグを直して再開

--- tenkijp.js
+++ tenkijp.js
@@ -37,8 +37,8 @@ async function getData(page, itemSelector) {
 async function getToday(page) {
   const hour = await getData(page, "#forecast-point-3h-today > tbody > tr.hour > td")
   // console.log(hour) //=> [ '03', '06', '09', '12', '15', '18', '21', '24' ]
-  const weatehr = await getData(page, "#forecast-point-3h-today > tbody > tr.weather > td")
-  // console.log(weatehr) //=> [ '雨', '曇り', '晴れ', '晴れ', '晴れ', '曇り', '曇り', '曇り' ]
+  const weather = await getData(page, "#forecast-point-3h-today > tbody > tr.weather > td")
+  // console.log(weather) //=> [ '雨', '曇り', '晴れ', '晴れ', '晴れ', '曇り', '曇り', '曇り' ]
   const windDirection = await getData(page, "#forecast-point-3h-today > tbody > tr.wind-direction > td > p")
   // console.log(windDirection) //=> [ '南西', '南', '南東', '南東', '東南東', '東南東', '東南東', '南東' ]
   const windSpeed = await getData(page, "#forecast-point-3h-today > tbody > tr.wind-speed > td")
@@ -48,7 +48,7 @@ async function getToday(page) {
   for (i = 1; i < 6; i++) {
     datas.push({
       hour: hour[i],
-      weatehr: weatehr[i],
+      weather: weather[i],
       windBlow: windDirection[i],
       windSpeed: windSpeed[i]
     })
@@ -65,8 +65,8 @@ async function getTomorrow(page) {
 
   const hour = await getData(page, "#forecast-point-3h-tomorrow > tbody > tr.hour > td")
   // console.log(hour) //=> [ '03', '06', '09', '12', '15', '18', '21', '24' ]
-  const weatehr = await getData(page, "#forecast-point-3h-tomorrow > tbody > tr.weather > td")
-  // console.log(weatehr) //=> [ '雨', '曇り', '晴れ', '晴れ', '晴れ', '曇り', '曇り', '曇り' ]
+  const weather = await getData(page, "#forecast-point-3h-tomorrow > tbody > tr.weather > td")
+  // console.log(weather) //=> [ '雨', '曇り', '晴れ', '晴れ', '晴れ', '曇り', '曇り', '曇り' ]
   const windDirection = await getData(page, "#forecast-point-3h-tomorrow > tbody > tr.wind-blow > td > p") //todayとtrクラス名が違う
   // console.log(windDirection) //=> [ '南西', '南', '南東', '南東', '東南東', '東南東', '東南東', '南東' ]
   const windSpeed = await getData(page, "#forecast-point-3h-tomorrow > tbody > tr.wind-speed > td")
@@ -75,7 +75,7 @@ async function getTomorrow(page) {
   for (i = 1; i < 6; i++) {
     datas.push({
       hour: hour[i],
-      weatehr: weatehr[i],
+      weather: weather[i],
       windBlow: windDirection[i],
       windSpeed: windSpeed[i]
     })
