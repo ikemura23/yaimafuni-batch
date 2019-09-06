e53f86c860c90bc538bab6641e2b262e16163ee9
ikemura <8417910+banbara23@users.noreply.github.com>
2019-09-06 09:11:19 +0900

    台風実装中

---
+++ weather/typhoon.js
@@ -0,0 +1,96 @@
+const puppeteer = require('puppeteer')
+const url = 'https://tenki.jp/lite/bousai/typhoon/'
+const firebase = require("firebase");
+const sendError = require('../slack');
+// Heroku環境かどうかの判断
+const LAUNCH_OPTION = process.env.DYNO ? { args: ['--no-sandbox', '--disable-setuid-sandbox'] } : { headless: true };
+
+module.exports = (async () => {
+  try {
+    const browser = await puppeteer.launch(LAUNCH_OPTION)
+    const page = await browser.newPage()
+    await page.goto(url, { waitUntil: 'domcontentloaded' }) // ページへ移動＋表示されるまで待機
+
+    const today = await getSection(page)
+
+    // const sendData = { today: today, tomorrow: tomorrow };
+    // await send(sendData)
+
+    browser.close()
+  } catch (err) {
+    console.error(err)
+    sendError(err.stack, "tenkijpのスクレイピングでエラー発生!")
+    browser.close()
+  }
+})
+
+async function getData(page, itemSelector) {
+  return datas = await page.evaluate((selector) => {
+    const list = Array.from(document.querySelectorAll(selector));
+    return list.map(data => data.textContent.trim());
+  }, itemSelector);
+}
+
+async function getData2(page, itemSelector) {
+  const nodes = await page.$$eval(itemSelector, list => {
+    const datas = list.filter((_, i) => i != 0 && i != list.length-1)
+    console.log(datas.length)
+    datas.forEach(data => {
+      console.log(data.textContent)
+    });
+    return {
+
+    }
+  })
+
+  console.log(nodes)
+}
+
+async function getData3(page,itemSelector) {
+  const nodes = await page.$(itemSelector)
+  const node = await nodes.$$("h3")
+  console.log(node.text)
+}
+
+/**
+ * 台風のセクションを取得、1番目は気象予報士の見解なので、スキップ
+ * ２番めから台風情報なので、２番め以降をとる
+ * 
+ * @param {Page} page 
+ */
+async function getSection(page) {
+  const datas = await getData3(page, "body > section > section.section-wrap")
+  // 最初と最後は不要データなので除外する [ '気象予報士の見解（日直予報士：台風関連記事）', '台風13号(レンレン)', '台風14号(カジキ)', '台風を知る' ]
+  // const data = notFilterData.filter((d, i) => i != 0 && i != notFilterData.length-1)
+
+  // const data2 = await getData2(page, "body > section > section.section-wrap")
+  // // console.log(hour) //=> [ '03', '06', '09', '12', '15', '18', '21', '24' ]
+  // const weather = await getData(page, "#forecast-point-3h-today > tbody > tr.weather > td")
+  // // console.log(weather) //=> [ '雨', '曇り', '晴れ', '晴れ', '晴れ', '曇り', '曇り', '曇り' ]
+  // const windDirection = await getData(page, "#forecast-point-3h-today > tbody > tr.wind-direction > td > p")
+  // // console.log(windDirection) //=> [ '南西', '南', '南東', '南東', '東南東', '東南東', '東南東', '南東' ]
+  // const windSpeed = await getData(page, "#forecast-point-3h-today > tbody > tr.wind-speed > td")
+  // // console.log(windSpeed) //=> [ '5', '5', '5', '5', '5', '5', '5', '4' ]
+  // const datas = []
+
+  // for (i = 1; i < 6; i++) {
+  //   datas.push({
+  //     hour: hour[i],
+  //     weather: weather[i],
+  //     windBlow: windDirection[i],
+  //     windSpeed: windSpeed[i]
+  //   })
+  // }
+  // console.log(data)
+  return datas
+}
+
+/**
+ * DBへ登録
+ */
+async function send(data) {
+  return Promise.all([
+    firebase.database().ref('weather/today/table').set(data.today),
+    firebase.database().ref('weather/tomorrow/table').set(data.tomorrow)
+  ])
+};
