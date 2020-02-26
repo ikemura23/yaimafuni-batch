6e86b4aa4cbc6474db0b3dc419c7c7910d47eaec
ikemura23 <banbara.work@gmail.com>
2020-02-27 08:04:50 +0900

    ykf詳細のportCode, headerまでできた

--- detail_new/detail_ykf.js
+++ detail_new/detail_ykf.js
@@ -21,7 +21,7 @@ module.exports = async () => {
     // スクレイピングした生の値、9件取得できるはず
     const listRaw = await getRawData(page);
     // 送信用に変換
-    const sendData = await convertSendData(listRaw);
+    const sendData = await convertSendData(page, listRaw);
 
     // 送信開始
     // await firebase.update(consts.YKF, sendData);
@@ -40,7 +40,32 @@ module.exports = async () => {
  * リスト取得
  */
 async function getRawData(page) {
-  return await getDataList(page, "#operationstatus > div > div.local");
+  console.log("getRawData")
+  const nodes = await page.$$("#operationstatus > div > div.local")
+
+  const datas = [];
+  for (const node of nodes) {
+    // 港名
+    const portName = await node.$eval('h3', nd => nd.innerText);
+    // ヘッダー左
+    const leftPortName = await node.$eval('tr:nth-child(2) > td:nth-child(1)', nd => nd.innerText);
+    // ヘッダー右
+    const rightPortName = await node.$eval('tr:nth-child(2) > td:nth-child(2)', nd => nd.innerText);
+    
+    const data = {
+      portCode: getPortCode(portName),
+      header: {
+        left: leftPortName,
+        right: rightPortName
+      },
+    }
+    console.log(data);
+    datas.push(data)
+  };
+
+  return datas;
+
+  // return await getDataList(page, "#operationstatus > div > div.local");
 }
 
 async function getDataList(page, itemSelector) {
@@ -53,21 +78,21 @@ async function getDataList(page, itemSelector) {
 /**
  * 生データから送信用の値を作成する
  */
-async function convertSendData(raws) {
-  // console.log(raws)
-  raws.map(raw => console.log(raw))
-  if (raws) return;
-  raws.foreach(raw => console.log(raw.textContent));
+async function convertSendData(page, nodes) {
+  // console.log(nodes)
+  // raws.map(raw => console.log(raw))
+  if (nodes) return;
+  nodes.foreach(node => console.log(node.textContent));
 
-  const sendData = list.map(text => {
-    const portName = text.slice(0, -1);
-    const statusRaw = text.slice(-1);
-    return {
-      portCode: getPortCode(portName),
-      portName: portName,
-      status: getStatusCode(statusRaw)
-    };
-  });
+  // const sendData = list.map(text => {
+  //   const portName = text.slice(0, -1);
+  //   const statusRaw = text.slice(-1);
+  //   return {
+  //     portCode: getPortCode(portName),
+  //     portName: portName,
+  //     status: getStatusCode(statusRaw)
+  //   };
+  // });
   return sendData;
 }
 
@@ -101,23 +126,23 @@ function getSendDataTemplete() {
 // 港名から港コードを返す
 function getPortCode(portName) {
   // 港id
-  if (portName === "竹富") {
+  if (portName === "竹富航路") {
     return consts.TAKETOMI;
-  } else if (portName === "小浜") {
+  } else if (portName === "小浜航路") {
     return consts.KOHAMA;
-  } else if (portName === "小浜-竹富") {
+  } else if (portName === "小浜-竹富航路") {
     return consts.KOHAMA_TAKETOMI;
-  } else if (portName === "黒島") {
+  } else if (portName === "黒島航路") {
     return consts.KUROSHIMA;
-  } else if (portName === "小浜-大原") {
+  } else if (portName === "小浜-大原航路") {
     return consts.KOHAMA_OOHARA;
-  } else if (portName === "西表大原") {
+  } else if (portName === "西表大原航路") {
     return consts.OOHARA;
-  } else if (portName === "西表上原") {
+  } else if (portName === "西表上原航路") {
     return consts.UEHARA;
-  } else if (portName === "上原-鳩間") {
+  } else if (portName === "上原-鳩間航路") {
     return consts.UEHARA_HATOMA;
-  } else if (portName === "鳩間") {
+  } else if (portName === "鳩間航路") {
     return consts.HATOMA;
   }
 }
