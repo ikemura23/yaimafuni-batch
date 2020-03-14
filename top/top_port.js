2218f118cf3d4978f511557518ea83389ac7d1c5
ikemura23 <banbara.work@gmail.com>
2020-03-14 16:06:37 +0900

    コードフォーマット適用

--- top/top_port.js
+++ top/top_port.js
@@ -1,42 +1,43 @@
 // 港別の運行ステータスの値をまとめているだけ
 
 const firebase = require("firebase");
-const consts = require('../consts.js');
+const consts = require("../consts.js");
 
 /**
  * メイン処理
  */
-module.exports = (async () => {
+module.exports = async () => {
+  console.log("開始 港別の作成 ");
 
-  console.log('開始 港別の作成 ')
+  const annei = await readDatabase(consts.ANEI);
+  const ykf = await readDatabase(consts.YKF);
+  const sendData = await createSendData(annei, ykf);
+  await sendToFirebase(sendData);
 
-const annei = await readDatabase(consts.ANEI)
-const ykf = await readDatabase(consts.YKF)
-const sendData = await createSendData(annei, ykf)
-await sendToFirebase(sendData)
-
-console.log('完了 港別の作成')
-})
+  console.log("完了 港別の作成");
+};
 
 /**
  * firebaeから取得した値を返す
  */
 async function readDatabase(path) {
-  return firebase.database()
+  return firebase
+    .database()
     .ref(path)
-    .once('value')
-    .then(function (snapshot) {
+    .once("value")
+    .then(function(snapshot) {
       return snapshot.val();
-    })
+    });
 }
 
 /**
  * firebaseへ送信
  */
 async function sendToFirebase(data) {
-  return firebase.database()
-    .ref('top_port')
-    .set(data)
+  return firebase
+    .database()
+    .ref("top_port")
+    .set(data);
 }
 
 /**
@@ -46,37 +47,37 @@ async function createSendData(anei, ykf) {
   // 竹富
   const taketomi = {
     anei: anei.taketomi,
-    ykf: ykf.taketomi,
-  }
+    ykf: ykf.taketomi
+  };
   // 小浜
   const kohama = {
     anei: anei.kohama,
-    ykf: ykf.kohama,
-  }
+    ykf: ykf.kohama
+  };
   // 黒島
   const kuroshima = {
     anei: anei.kuroshima,
-    ykf: ykf.kuroshima,
-  }
+    ykf: ykf.kuroshima
+  };
   // 大原
   const oohara = {
     anei: anei.oohara,
-    ykf: ykf.oohara,
-  }
+    ykf: ykf.oohara
+  };
   // 上原
   const uehara = {
     anei: anei.uehara,
-    ykf: ykf.uehara,
-  }
+    ykf: ykf.uehara
+  };
   // 鳩間
   const hatoma = {
     anei: anei.hatoma,
-    ykf: ykf.hatoma,
-  }
+    ykf: ykf.hatoma
+  };
   // 波照間
   const hateruma = {
     anei: anei.hateruma
-  }
+  };
   return {
     taketomi,
     kohama,
@@ -85,5 +86,5 @@ async function createSendData(anei, ykf) {
     uehara,
     hatoma,
     hateruma
-  }
+  };
 }
