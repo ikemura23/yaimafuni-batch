ea702a2c4b1cc8d5fb68561780521960b6d1c0fe
ikemura23 <banbara.work@gmail.com>
2020-03-14 15:34:16 +0900

    config.jsからncmbを削除

--- config/config.js
+++ config/config.js
@@ -8,10 +8,6 @@ module.exports = {
   slack: {
     "webHookUrl": "https://hooks.slack.com/services/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"
   },
-  ncmb: {
-    apiKey: "99f9ad328e9a72cfc482902a8a8b61af7d24a1516ed79e2946aa47b74b61a117",
-    clientKey: "ea1485bf126b16b0d5dbb3138e9d7c61643730dd96320750bf4f0c21cf474c03"
-  },
   puppeteer: {
     userAgent:"Mozilla /5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B5110e Safari/601.1",
   }
