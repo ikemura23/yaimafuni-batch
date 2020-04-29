ad6b7540fce6e03535b1d34dce2db2f250fb682a
ikemura23 <banbara.work@gmail.com>
2020-04-30 00:26:55 +0900

    sendErrorが使われてないので使用

--- detail/detail_anei.js
+++ detail/detail_anei.js
@@ -6,7 +6,7 @@ const LAUNCH_OPTION = process.env.DYNO
   : { headless: true };
 const URL = "http://www.aneikankou.co.jp/timetables";
 const consts = require("../consts.js");
-const config = require("../config/config");
+// const config = require("../config/config");
 const firebase = require("../lib/firebase_repository");
 const sendError = require("../slack");
 const COMPANY = consts.ANEI;
@@ -23,8 +23,8 @@ module.exports = async () => {
 
     browser.close();
   } catch (error) {
-    console.error(error.stack, `${COMPANY}一覧でエラー`);
-    // sendError(error.stack, `${COMPANY}一覧のスクレイピングでエラー発生!`);
+    console.error(error.stack, `${COMPANY}詳細でエラー`);
+    sendError(error.stack, `${COMPANY} 詳細のスクレイピングでエラー発生!`);
     browser.close();
   } finally {
     console.log(`終了: ${COMPANY} 詳細`);
