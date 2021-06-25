83f7cba09f4d92d882c933533653b3821b3a47f3
ikemura23 <banbara.work@gmail.com>
2021-06-25 10:23:20 +0900

    getAnneiUpdateTimeAndCommentを組み込む

--- src/list/updateAnneiList.js
+++ src/list/updateAnneiList.js
@@ -1,9 +1,22 @@
+const getAnneiUpdateTimeAndComment = require("./getAnneiUpdateTimeAndComment");
 const getAnneiList = require("./getAnneiList");
 const saveAnneiList = require("./saveAnneiList");
 
 const updateAnneiList = async () => {
-    const value = await getAnneiList();
-    await saveAnneiList(value)
+  const updateTimeAndComment = await getAnneiUpdateTimeAndComment();
+  const value = await getAnneiList();
+  const saveData = {
+    updateTime: updateTimeAndComment.updateTime,
+    comment: updateTimeAndComment.comment,
+    hateruma: null,
+    hatoma: null,
+    kohama: null,
+    kuroshima: null,
+    oohara: null,
+    taketomi: null,
+    uehara: null,
+  };
+  await saveAnneiList(saveData);
 };
 
 module.exports = updateAnneiList;
