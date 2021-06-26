c17f699a4c924604d8e995e36a9bd4a890c1d631
ikemura23 <banbara.work@gmail.com>
2021-06-26 18:37:36 +0900

    updateAnneiDetail.js を作成

---
+++ src/detail/updateAnneiDetail.js
@@ -0,0 +1,17 @@
+const getAnneiDetail = require("./getAnneiDetail");
+const saveAnneiDetail = require("./saveAnneiDetail");
+
+const updateAnneiDetail = async () => {
+  console.group("updateAnneiDetail start");
+
+  // 取得
+  const value = await getAnneiDetail();
+  
+  // 保存
+  await saveAnneiDetail(value);
+  
+  console.log("updateAnneiDetail end");
+  console.groupEnd();
+};
+
+module.exports = updateAnneiDetail;
