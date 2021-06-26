7822a829f774b04041f9ee1e1decbc7f19762172
ikemura23 <banbara.work@gmail.com>
2021-06-26 23:57:34 +0900

    repositoryをsrcに移動

--- src/list/ykf/ykf_time_and_announce.js
+++ src/list/ykf/ykf_time_and_announce.js
@@ -6,7 +6,7 @@ const LAUNCH_OPTION = process.env.DYNO
 const URL = "https://www.yaeyama.co.jp/operation.html";
 const consts = require("../../../consts.js");
 const config = require("../../config/config");
-const firebase = require("../../../lib/firebase_repository");
+const firebase = require("../../repository/firebase_repository");
 
 const COMPANY = consts.YKF;
 
