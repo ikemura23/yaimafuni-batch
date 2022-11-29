e64480d260a40ed4c7a1dec93dcb3d0d842b72c0
ikemura23 <banbara.work@gmail.com>
2022-11-29 17:17:05 +0900

    executablePathに変更

--- src/browser-factory.js
+++ src/browser-factory.js
@@ -4,7 +4,7 @@ async function create() {
   const LAUNCH_OPTION = {
     headless: true,
     // channel: "chrome", // Mac
-    channel: 'chromium-browser"', // Raspberry Pi
+    executablePath: "chromium-browser", // Raspberry Pi
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   };
 //   const browser = await puppeteer.launch(LAUNCH_OPTION);
