cc361adc107e81aaec02129da05db4000c6fe250
ikemura23 <banbara.work@gmail.com>
2022-12-12 15:34:38 +0900

    src/weather/getWeatherForecastApi.js を作成

---
+++ src/weather/getWeatherForecastApi.js
@@ -0,0 +1,86 @@
+const url =
+  "https://api.open-meteo.com/v1/jma?latitude=24.34&longitude=124.16&hourly=weathercode,windspeed_10m,winddirection_10m&current_weather=true&windspeed_unit=ms&timezone=Asia%2FTokyo";
+
+(async () => {
+  const json = await (await fetch(url)).json();
+  const parsedObj = JSON.parse(JSON.stringify(json)); // JSON.stringifyがないとパースエラーになる
+
+  const data = mapToData(
+    parsedObj.hourly.time,
+    parsedObj.hourly.weathercode,
+    parsedObj.hourly.windspeed_10m,
+    parsedObj.hourly.winddirection_10m
+  );
+  const dateGroupList = groupBy(data, "date");
+  console.log(dateGroupList);
+})();
+
+/**
+ * APIレスポンス Jsonからオブジェクト型に変換する
+ * @param {Array} timeList 日時
+ * @param {Array} weatherCodeList 天気コード
+ * @param {Array} windSpeedList 風速
+ * @param {Array} windDirectionList 風向き
+ * @returns {Object} 必要な値(日時、天気コード、風速、風向き)のみの値
+ */
+function mapToData(
+  timeList,
+  weatherCodeList,
+  windSpeedList,
+  windDirectionList
+) {
+  const result = [];
+
+  timeList.forEach((time, i) => {
+    result[i] = {
+      date: time.substring(0, 10), // YYYY-MM-DD までを取得して、後にgorupByのキーに使う
+      dateTime: time,
+      weatherCode: weatherCodeList[i],
+      cardinalDirection: calculateCardinalDirection(windDirectionList[i]),
+      windSpeed: windSpeedList[i],
+    };
+  });
+  return result;
+}
+
+/**
+ * 風向の数値の値から、方角を計算して返す
+ * @param {String} windDirection 風向の値、0〜360の間になる
+ * @returns {string} 方角
+ */
+function calculateCardinalDirection(windDirection) {
+  const cardinalDirection = [
+    "北",
+    "北北東",
+    "北東",
+    "東北東",
+    "東",
+    "東南東",
+    "南東",
+    "南南東",
+    "南",
+    "南南西",
+    "南西",
+    "西南西",
+    "西",
+    "西北西",
+    "北西",
+    "北北西",
+    "北",
+  ];
+  const directionIndex = Math.round(windDirection / 22.5);
+  return cardinalDirection[directionIndex];
+}
+
+/**
+ * 指定された値(key)でグルーピングして返す
+ * @param {Object} xs
+ * @param {string} key グルーピングする値
+ * @returns {Array<Object>} グルーピングされたList
+ */
+const groupBy = function (xs, key) {
+  return xs.reduce(function (rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+};
