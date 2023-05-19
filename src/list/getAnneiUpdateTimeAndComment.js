7595df64ca7b31d0636025811744b431abe08128
ikemura23 <banbara.work@gmail.com>
2023-05-19 10:50:05 +0900

    refact: 開発用ログ出力を削除

--- src/list/getAnneiUpdateTimeAndComment.js
+++ src/list/getAnneiUpdateTimeAndComment.js
@@ -5,18 +5,11 @@ const TARGET_URL = "https://aneikankou.co.jp/condition";
 
 const getAnneiUpdateTimeAndComment = async () => {
     console.group("getAnneiUpdateTimeAndComment start");
-
-    console.log("await createBrowser()")
     const browser = await createBrowser();
-    console.log("await browserFactory.create() end")
     try {
-        console.log("try start")
         const page = await browser.newPage();
-        console.log("browser.newPage() ok")
-
         await page.goto(TARGET_URL, {waitUntil: "networkidle2"}); // ページへ移動＋表示されるまで待機
-        console.log("page.goto ok: ", TARGET_URL)
-        console.log("更新時刻の取得")
+
         // 更新時刻の取得
         const updateTimeText = await page.$eval(
             "div.condition_subtitle > div",
@@ -27,7 +20,6 @@ const getAnneiUpdateTimeAndComment = async () => {
                     .trim();
             }
         );
-        console.log("お知らせコメントの取得")
         // お知らせコメントの取得
         const commentText = await page.$eval("div.condition_list > div", (item) => {
             return item.textContent.trim();
