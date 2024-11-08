2eddf13412b602435ea0b3a35d679eaece0843b7
Kazutaka Ikemura <banbara.work@gmail.com>
2024-11-08 23:28:06 +0900

    feat: 試しにfirestoreにも安栄を送ってみる

--- src/list/saveAnneiList.js
+++ src/list/saveAnneiList.js
@@ -1,12 +1,14 @@
 const repository = require("../repository/firebase_repository");
+const firestoreRepository = require("../repository/firestoreRepository.js");
 const consts = require("../consts.js");
 
 const saveAnneiList = async (value) => {
-    console.group("saveAnneiList start");
+  console.group("saveAnneiList start");
 
-    await repository.set(consts.ANEI, value);
+  await repository.set(consts.ANEI, value);
+  await firestoreRepository.set(consts.ANEI, value);
 
-    console.groupEnd();
+  console.groupEnd();
 };
 
 module.exports = saveAnneiList;
