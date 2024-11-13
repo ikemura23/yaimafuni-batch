c56ae0c70140adede78613584f07a498e41ede3b
Kazutaka Ikemura <banbara.work@gmail.com>
2024-11-13 15:45:45 +0900

    refactor: executablePathの取得を変数に格納

--- src/browser-factory.js
+++ src/browser-factory.js
@@ -2,9 +2,11 @@ const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium");
 
 module.exports = async () => {
+  const executablePath =
+    process.env.CHROMIUM_EXECUTABLE_PATH ?? (await chromium.executablePath());
+
   const LAUNCH_OPTION = {
-    executablePath:
-      process.env.CHROMIUM_EXECUTABLE_PATH ?? (await chromium.executablePath()),
+    executablePath: executablePath,
     headless: process.env.IS_LOCAL ? true : chromium.headless,
     ignoreHTTPSErrors: true,
     defaultViewport: chromium.defaultViewport,
